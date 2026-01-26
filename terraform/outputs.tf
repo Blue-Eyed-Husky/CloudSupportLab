cf350bd6d8feb121d9b3d7edab42921ed10f2a80
Mitchell <chochhay@yahoo.com>
2026-01-25 17:47:31 -0700

    added terraform.tfvars and outputs.tf update

---
+++ terraform/outputs.tf
@@ -0,0 +1,29 @@
+output "isntance_public_ip" {
+description = "Public IP of the EC2 instance"
+  value = aws_instance.cloud_lab_instance.public_ip
+}
+
+output "instnace_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.cloud_lab_instance.public_dns
+}
+
+output "nginx_url" {
+  description = "URL to access NGINX server"
+  value       = "http://${aws_instance.cloud_lab_instance.public_ip}"
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.cloud_lab_vpc.id
+}
+
+output "public_subnet_id" {
+  description = "Public Subnet ID"
+  value       = aws_subnet.cloud_lab_subnet.id
+}
+
+output "security_group_id" {
+  description = "Security Group ID"
+  value       = aws_security_group.cloud_lab_sg.id
+}
