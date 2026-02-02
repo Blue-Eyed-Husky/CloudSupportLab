e5de661a3964ba4e85c88db36f21ecd22727b1e1
Mitchell <chochhay@yahoo.com>
2026-02-02 15:11:11 -0700

    added rsync to user_data

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -eux
+set -euo pipefail
 
 apt-get update -y
-apt-get install -y nginx
+apt-get install -y nginx rsync
 
 snap install aws-cli --classic
 
@@ -11,7 +11,4 @@ systemctl start nginx
 
 echo "<h1>Cloud Support Lab by Cho- NGINX is running</h1>" > /var/www/html/index.html
 
-# Test S3 access using the instance role creds (no keys stored)
-echo "hello from cloud lab instance" > /tmp/hello.txt
-aws s3 cp /tmp/hello.txt ${my_s3_bucket}/hello_from_instance.txt
-aws s3 ls ${my_s3_bucket}/
+
